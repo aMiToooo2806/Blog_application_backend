d0d847799ec81ea2a6f4b4e673e3afb64257936f
aMiToooo2806 <amitnarwade56@gmail.com>
2025-02-13 17:03:00 +0530

    checking

--- src/main/java/com/amit/BlogApplication/controllers/UserController.java
+++ src/main/java/com/amit/BlogApplication/controllers/UserController.java
@@ -1,2 +1,23 @@
-package com.amit.BlogApplication.controllers;public class UserController {
+package com.amit.BlogApplication.controllers;
+
+import com.amit.BlogApplication.payloads.UserDto;
+import com.amit.BlogApplication.services.Impl.UserServiceImpl;
+import com.amit.BlogApplication.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    private ResponseEntity<UserDto>createUser(@RequestBody UserDto userDto)
+    {
+        UserDto createdUser = userService.createUser(userDto);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/amit/BlogApplication/entities/Users.java
+++ src/main/java/com/amit/BlogApplication/entities/Users.java
@@ -9,9 +9,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@NoArgsConstructor
-@Getter
-@Setter
 public class Users {
 
     @Id
@@ -25,4 +22,47 @@ public class Users {
     private String password;
 
     private String about;
+
+    public Users() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
 }
