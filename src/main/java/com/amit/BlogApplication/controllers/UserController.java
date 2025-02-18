39da69fc6a97bdb8cea09da42f06e8aba13962d2
aMiToooo2806 <amitnarwade56@gmail.com>
2025-02-18 23:14:09 +0530

    Completed User controller and it is working

--- src/main/java/com/amit/BlogApplication/controllers/UserController.java
+++ src/main/java/com/amit/BlogApplication/controllers/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -15,9 +18,36 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/create")
-    private ResponseEntity<UserDto>createUser(@RequestBody UserDto userDto)
+    public ResponseEntity<UserDto>createUser(@RequestBody UserDto userDto)
     {
         UserDto createdUser = userService.createUser(userDto);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("update/{userId}")
+    public ResponseEntity<?>updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId)
+    {
+        UserDto updatedUser = this.userService.updateUser(userDto, userId);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("delete/{userId}")
+    public ResponseEntity<?>deleteUser(@PathVariable Integer userId)
+    {
+        this.userService.deleteUser(userId);
+        return new ResponseEntity<>(Map.of("Message","User deleted successfully"),HttpStatus.OK);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<UserDto>>getAllUsers()
+    {
+        return ResponseEntity.ok(this.userService.getAllUsers());
+    }
+
+    @GetMapping("ById/{userId}")
+    private ResponseEntity<UserDto>getUserById(@PathVariable Integer userId)
+    {
+        UserDto userById = this.userService.getUserById(userId);
+        return ResponseEntity.ok(userById);
     }
 }
